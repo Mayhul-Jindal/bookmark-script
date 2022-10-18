678d7c9274a3f4d24e2f6b6518a7f28ef27a8d4c
Mayhul-Jindal <mayhuljindal@gmail.com>
2022-10-19 03:45:31 +0530

    Is able to accept post request and ngrok connection

--- server-V1/server.js
+++ server-V1/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const cors = require('cors')
 
 const app = express();
 
@@ -9,15 +8,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // cors
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (req, res) => {
     res.json({"message":"hello world"})
 })
 
-app.post('/send', (req,res) => {
+app.post('/sendBookmarks', (req,res) => {
     console.log(req.body)
     res.status(200).json({"message":"Recieved"})
 })
-  
-app.listen(5000, () => console.log('Server is up and running on port 5000'))
+
+app.listen(9999, () => console.log('Server is up and running on port 9999'))
