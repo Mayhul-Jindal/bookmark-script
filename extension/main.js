3c560f3b7340d1080ec2eb2d83bf1bb3640c665b
Mayhul-Jindal <mayhuljindal@gmail.com>
2022-10-19 03:41:27 +0530

    feat: sends all bookmarks for now

--- extension/main.js
+++ extension/main.js
@@ -1,52 +1,83 @@
-let helperBool = false
+let count = 0
 
-async function getBookmark(){
-    let temp = await browser.bookmarks.getRecent(1)
-    let parentInfo = await browser.bookmarks.get(temp[0].parentId)
-    let test = {
-        name: temp[0].title,
-        url: temp[0].url,
-        topic: parentInfo[0].title
+async function countBookmarks(){
+
+  // This part recursively checks for all existing childrens and updates count to total number of bookmarks
+  function logItems(bookmarkItem) {
+    if (bookmarkItem.url) {
+      count++;
     }
+    if (bookmarkItem.children) {
+      for (child of bookmarkItem.children) {
+        logItems(child);
+      }
+    }
+  }
+  
+  function logTree(bookmarkItems) {
+    logItems(bookmarkItems[0]);
+  }
+ 
+  let gettingTree = await browser.bookmarks.getTree();
 
-    return test
+  // handling error
+  try {
+    logTree(gettingTree)
+    return count
+  } catch (error) {
+    return error
+  }
+  
 }
 
-async function postReq(data = {}){
-  const response = await fetch('https://5093-182-79-4-250.in.ngrok.io/send', {
+async function getBookmarks(index){
+    var arrayOfBookmarks = [];
+
+    let recents = await browser.bookmarks.getRecent(index) // example ke liye 10 deh diye
+    for (let i = 1; i < recents.length; i++) {
+      let bookmark = recents[i];
+      let parentInfo = await browser.bookmarks.get(bookmark.parentId)
+      let temp = {
+        name: bookmark.title,
+        url: bookmark.url,
+        topic: parentInfo[0].title
+      }
+      arrayOfBookmarks.push(temp)
+    } 
+    return arrayOfBookmarks
+}
+
+// function used to send and recieve data to server
+async function getReq(path) {
+  const response = await fetch(`https://88c7-185-183-33-221.eu.ngrok.io/${path}`,{
+    method: 'GET',
+    mode: 'cors',
+  })
+  return data
+}
+async function postReq(path, data){
+  const response = await fetch(`https://88c7-185-183-33-221.eu.ngrok.io/${path}`, {
     method: 'POST',
     mode: 'cors',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data) // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
-async function handleBookmarkEvent(_, bookmarkInfo) {
-    let result = await getBookmark()
-    let response = await postReq(result)
-    console.log(response)
-    // if (bookmarkInfo.type == 'folder') {
-    //     helperBool = false
-    //     console.log("folder detected")
-    // }
-    // else if(bookmarkInfo.type == 'bookmark' && helperBool == true){
-    //     console.log(getBookmark())
-    //     // let parentInfo = await browser.bookmarks.get(bookmarkInfo.parentId)
-    //     // console.log(`Your bookmark Name ${bookmarkInfo.title}}`)
-    //     // console.log(`Your bookmark URL ${bookmarkInfo.url}`)
-    //     // console.log(`Your bookmark topic ${parentInfo[0].title}`)
-    // }
-    // else{
-    //     console.log("Skipping this bookmark event to get correct bookmark topic")
-    //     helperBool = true
-    // }
-}
+async function handleBookmarkEvent() {
 
+  // sending bookmarks
+  let totalBookmarks = await countBookmarks();
+  let result = await getBookmarks(totalBookmarks)
+  let response = await postReq("sendBookmarks",result)
+  console.log(response)
+
+}
 
 // This will run when a bookmark is created.
 browser.bookmarks.onCreated.addListener(
-    handleBookmarkEvent
+  handleBookmarkEvent
 );
